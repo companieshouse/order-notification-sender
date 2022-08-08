1ee67f37f22c0a4b802f9a63e9c153b573c1e001
kinpang-CH <kpang@companieshouse.gov.uk>
2022-08-08 16:22:26 +0100

    Implement CertifiedCopyEmailDataMapper
    * Map the correct value for data provided by from BaseItemApi
    * Seperate private method to map filing history document values

--- src/main/java/uk/gov/companieshouse/ordernotification/emailsendmodel/CertifiedCopy.java
+++ src/main/java/uk/gov/companieshouse/ordernotification/emailsendmodel/CertifiedCopy.java
@@ -1,13 +1,23 @@
 package uk.gov.companieshouse.ordernotification.emailsendmodel;
 
+import java.util.List;
+import java.util.Objects;
+
 public class CertifiedCopy {
     private String id;
-    private String dateFiled;
-    private String type;
-    private String description;
     private String companyNumber;
     private String deliveryMethod;
-    private String fee;
+    private List<FilingHistoryDetailsModel> filingHistoryDetailsModelList;
+
+    public CertifiedCopy(String id,
+            String companyNumber,
+            String deliveryMethod,
+            List<FilingHistoryDetailsModel> filingHistoryDetailsModelList) {
+        this.id = id;
+        this.companyNumber = companyNumber;
+        this.deliveryMethod = deliveryMethod;
+        this.filingHistoryDetailsModelList = filingHistoryDetailsModelList;
+    }
 
     public String getId() {
         return id;
@@ -17,36 +27,8 @@ public class CertifiedCopy {
         this.id = id;
     }
 
-    public String getDateFiled() {
-        return dateFiled;
-    }
-
-    public void setDateFiled(String dateFiled) {
-        this.dateFiled = dateFiled;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public String getCompanyNumber() {
         return companyNumber;
-    }
-
-    public void setCompanyNumber(String companyNumber) {
-        this.companyNumber = companyNumber;
     }
 
     public String getDeliveryMethod() {
@@ -57,11 +39,82 @@ public class CertifiedCopy {
         this.deliveryMethod = deliveryMethod;
     }
 
-    public String getFee() {
-        return fee;
+    public void setCompanyNumber(String companyNumber) {
+        this.companyNumber = companyNumber;
     }
 
-    public void setFee(String fee) {
-        this.fee = fee;
+    public List<FilingHistoryDetailsModel> getFilingHistoryDetailsModelList() {
+        return filingHistoryDetailsModelList;
+    }
+
+    public void setFilingHistoryDetailsModelList(List<FilingHistoryDetailsModel> filingHistoryDetailsModelList) {
+        this.filingHistoryDetailsModelList = filingHistoryDetailsModelList;
+    }
+
+    public static CertifiedCopyBuilder builder() {
+        return new CertifiedCopyBuilder();
+    }
+
+    static class CertifiedCopyBuilder implements Cloneable {
+        private String id;
+        private String companyNumber;
+        private String deliveryMethod;
+        private List<FilingHistoryDetailsModel> filingHistoryDetailsModelList;
+
+        @Override
+        protected CertifiedCopyBuilder clone() {
+            try {
+                return (CertifiedCopyBuilder) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public CertifiedCopyBuilder withId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public CertifiedCopyBuilder withCompanyNumber(String companyNumber) {
+            this.companyNumber = companyNumber;
+            return this;
+        }
+
+        public CertifiedCopyBuilder withDeliveryMethod(String deliveryMethod) {
+            this.deliveryMethod = deliveryMethod;
+            return this;
+        }
+
+        public CertifiedCopyBuilder withFilingHistoryDetailsModelList(
+                List<FilingHistoryDetailsModel> filingHistoryDetailsModelList) {
+            this.filingHistoryDetailsModelList = filingHistoryDetailsModelList;
+            return this;
+        }
+
+        public CertifiedCopy build() {
+            return new CertifiedCopy(id, companyNumber, deliveryMethod,
+                    filingHistoryDetailsModelList);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            CertifiedCopyBuilder that = (CertifiedCopyBuilder) o;
+            return Objects.equals(id, that.id) &&
+                    Objects.equals(companyNumber, that.companyNumber) &&
+                    Objects.equals(deliveryMethod, that.deliveryMethod) &&
+                    Objects.equals(filingHistoryDetailsModelList,
+                            that.filingHistoryDetailsModelList);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, companyNumber, deliveryMethod, filingHistoryDetailsModelList);
+        }
     }
 }
---
+++ src/main/java/uk/gov/companieshouse/ordernotification/emailsendmodel/CertifiedCopyEmailDataMapper.java
@@ -0,0 +1,58 @@
+package uk.gov.companieshouse.ordernotification.emailsendmodel;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+import uk.gov.companieshouse.api.model.order.item.BaseItemApi;
+import uk.gov.companieshouse.api.model.order.item.CertifiedCopyItemOptionsApi;
+import uk.gov.companieshouse.ordernotification.config.EmailConfiguration;
+
+public class CertifiedCopyEmailDataMapper {
+    private final DeliveryMethodMapper deliveryMethodMapper;
+    private final FilingHistoryDescriptionProviderService providerService;
+    private EmailConfiguration emailConfiguration;
+
+    public CertifiedCopyEmailDataMapper(DeliveryMethodMapper deliveryMethodMapper,
+            FilingHistoryDescriptionProviderService providerService,
+            EmailConfiguration emailConfiguration) {
+        this.deliveryMethodMapper = deliveryMethodMapper;
+        this.providerService = providerService;
+        this.emailConfiguration = emailConfiguration;
+    }
+
+    CertifiedCopy map(BaseItemApi certifiedDocumentItem) {
+        CertifiedCopyItemOptionsApi itemOptions =
+                (CertifiedCopyItemOptionsApi) certifiedDocumentItem.getItemOptions();
+
+        return CertifiedCopy.builder()
+                .withId(certifiedDocumentItem.getId())
+                .withCompanyNumber(certifiedDocumentItem.getCompanyNumber())
+                .withDeliveryMethod(deliveryMethodMapper.mapDeliveryMethod(
+                        itemOptions.getDeliveryMethod(), itemOptions.getDeliveryTimescale()))
+                .withFilingHistoryDetailsModelList(mapFilingHistoryDocuments(itemOptions))
+                .build();
+    }
+
+    private List<FilingHistoryDetailsModel> mapFilingHistoryDocuments(
+            CertifiedCopyItemOptionsApi itemOptions) {
+        return itemOptions.getFilingHistoryDocuments().stream()
+            .map(filingHistoryDocumentApi -> {
+                FilingHistoryDetailsModel details = new FilingHistoryDetailsModel();
+                details.setFilingHistoryDate(
+                        LocalDate.parse(filingHistoryDocumentApi.getFilingHistoryDate())
+                        .format(DateTimeFormatter.ofPattern(emailConfiguration.getDocument()
+                                .getFilingHistoryDateFormat())));
+                details.setFilingHistoryCost(
+                        "£" + filingHistoryDocumentApi.getFilingHistoryCost());
+                details.setFilingHistoryDescription(
+                        this.providerService.mapFilingHistoryDescription(
+                                filingHistoryDocumentApi.getFilingHistoryDescription(),
+                                filingHistoryDocumentApi.getFilingHistoryDescriptionValues()
+                        )
+                );
+                details.setFilingHistoryType(filingHistoryDocumentApi.getFilingHistoryType());
+                return details;
+            }).collect(Collectors.toList());
+    }
+}
