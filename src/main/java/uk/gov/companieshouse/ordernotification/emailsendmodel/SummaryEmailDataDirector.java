9d0180ead87be7033abf0049005ad0be090d9308
cgregoryCH <cgregory@companieshouse.gov.uk>
2022-08-08 15:56:49 +0100

    Implement mapping algorithm in email data director
    * Delegates mapping to converter method depending on item kind
    * Converter annotated as prototype scope and interface proxy mode
    * as to use seperate converter data per call to director map

--- src/main/java/uk/gov/companieshouse/ordernotification/emailsendmodel/SummaryEmailDataDirector.java
+++ src/main/java/uk/gov/companieshouse/ordernotification/emailsendmodel/SummaryEmailDataDirector.java
@@ -1,9 +1,33 @@
 package uk.gov.companieshouse.ordernotification.emailsendmodel;
 
+import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.model.order.OrdersApi;
+import uk.gov.companieshouse.api.model.order.item.BaseItemApi;
+import uk.gov.companieshouse.ordernotification.emailsender.NonRetryableFailureException;
 
+@Component
 public class SummaryEmailDataDirector {
-    public OrderNotificationEmailData map(OrdersApi ordersApi){
-        return null;
+
+    private final OrderNotificationDataConvertable converter;
+
+    public SummaryEmailDataDirector(OrderNotificationDataConvertable converter) {
+        this.converter = converter;
+    }
+
+    public OrderNotificationEmailData map(OrdersApi ordersApi) {
+        converter.mapOrder(ordersApi);
+        for (BaseItemApi itemApi: ordersApi.getItems()) {
+            String kind = itemApi.getKind();
+            if ("item#certificate".equals(kind)) {
+                converter.mapCertificate(itemApi);
+            } else if ("item#certified-copy".equals(kind)) {
+                converter.mapCertifiedCopy(itemApi);
+            } else if ("item#missing-image-delivery".equals(kind)) {
+                converter.mapMissingImageDelivery(itemApi);
+            } else {
+                throw new NonRetryableFailureException(String.format("Unhandled kind: [%s]", kind));
+            }
+        }
+        return converter.getEmailData();
     }
 }
---
+++ src/test/java/uk/gov/companieshouse/ordernotification/emailsendmodel/SummaryEmailDataDirectorTest.java
@@ -0,0 +1,94 @@
+package uk.gov.companieshouse.ordernotification.emailsendmodel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collections;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import uk.gov.companieshouse.api.model.order.OrdersApi;
+import uk.gov.companieshouse.api.model.order.item.BaseItemApi;
+import uk.gov.companieshouse.ordernotification.config.TestConfig;
+import uk.gov.companieshouse.ordernotification.emailsender.NonRetryableFailureException;
+
+@SpringBootTest
+@Import(TestConfig.class)
+@TestPropertySource(locations="classpath:application-stubbed.properties")
+@ActiveProfiles("feature-flags-disabled")
+@ExtendWith(MockitoExtension.class)
+class SummaryEmailDataDirectorTest {
+
+    @Autowired
+    private SummaryEmailDataDirector summaryEmailDataDirector;
+
+    @MockBean
+    private OrderNotificationDataConvertable orderNotificationDataConvertable;
+
+    @Mock
+    private OrdersApi ordersApi;
+
+    @Mock
+    private BaseItemApi certificate;
+
+    @Mock
+    private BaseItemApi certifiedCopy;
+
+    @Mock
+    private BaseItemApi missingImageDelivery;
+
+    @Mock
+    private OrderNotificationEmailData emailData;
+
+    @Test
+    @DisplayName("test maps item kinds to the correct mapper methods")
+    void testMapSuccess() {
+        // given
+        when(certificate.getKind()).thenReturn("item#certificate");
+        when(certifiedCopy.getKind()).thenReturn("item#certified-copy");
+        when(missingImageDelivery.getKind()).thenReturn("item#missing-image-delivery");
+        when(ordersApi.getItems()).thenReturn(Arrays.asList(certificate, certifiedCopy, missingImageDelivery));
+        when(orderNotificationDataConvertable.getEmailData()).thenReturn(emailData);
+
+        // when
+        OrderNotificationEmailData actual = summaryEmailDataDirector.map(ordersApi);
+
+        // then
+        assertEquals(emailData, actual);
+        verify(orderNotificationDataConvertable).mapOrder(ordersApi);
+        verify(orderNotificationDataConvertable).mapCertificate(certificate);
+        verify(orderNotificationDataConvertable).mapCertifiedCopy(certifiedCopy);
+        verify(orderNotificationDataConvertable).mapMissingImageDelivery(missingImageDelivery);
+    }
+
+    @Test
+    @DisplayName("test throws non-retryable exception when handling an unknown item kind")
+    void testMapFailure() {
+        // given
+        when(certificate.getKind()).thenReturn("incorrect-kind");
+        when(ordersApi.getItems()).thenReturn(Collections.singletonList(certificate));
+
+        // when
+        Executable actual = () -> summaryEmailDataDirector.map(ordersApi);
+
+        // then
+        NonRetryableFailureException exception = assertThrows(NonRetryableFailureException.class, actual);
+        assertEquals("Unhandled kind: [incorrect-kind]", exception.getMessage());
+        verify(orderNotificationDataConvertable).mapOrder(ordersApi);
+        verifyNoMoreInteractions(orderNotificationDataConvertable);
+    }
+}
