3e143e716f01a37b9377279b8ffae8906fad2f34
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-11-21 14:56:57 +0000

    DCAC-222: Inject logger.

--- src/main/java/uk/gov/companieshouse/ordernotification/ordersconsumer/ItemGroupProcessedSendHandler.java
+++ src/main/java/uk/gov/companieshouse/ordernotification/ordersconsumer/ItemGroupProcessedSendHandler.java
@@ -1,34 +1,30 @@
 package uk.gov.companieshouse.ordernotification.ordersconsumer;
 
-import static uk.gov.companieshouse.ordernotification.logging.LoggingUtilsConfiguration.APPLICATION_NAMESPACE;
-
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.itemgroupprocessedsend.ItemGroupProcessedSend;
 import uk.gov.companieshouse.logging.Logger;
-import uk.gov.companieshouse.logging.LoggerFactory;
 import uk.gov.companieshouse.ordernotification.ordernotificationsender.SendOrderNotificationEventFactory;
-
 
 @Service
 public class ItemGroupProcessedSendHandler implements ApplicationEventPublisherAware {
 
     private final SendOrderNotificationEventFactory eventFactory;
-
+    private final Logger logger;
     private ApplicationEventPublisher applicationEventPublisher;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(APPLICATION_NAMESPACE);
-
-    public void handleMessage(Message<ItemGroupProcessedSend> message) {
-        LOGGER.info("processing item-group-processed-send message: " + message.getPayload());
-
-        // TODO: Write a handler for this (another ticket?)
+    public ItemGroupProcessedSendHandler(SendOrderNotificationEventFactory eventFactory,
+        Logger logger) {
+        this.eventFactory = eventFactory;
+        this.logger = logger;
     }
 
-    public ItemGroupProcessedSendHandler(SendOrderNotificationEventFactory eventFactory) {
-        this.eventFactory = eventFactory;
+    public void handleMessage(Message<ItemGroupProcessedSend> message) {
+        logger.info("processing item-group-processed-send message: " + message.getPayload());
+
+        // TODO: Write a handler for this (another ticket?)
     }
 
     @Override
