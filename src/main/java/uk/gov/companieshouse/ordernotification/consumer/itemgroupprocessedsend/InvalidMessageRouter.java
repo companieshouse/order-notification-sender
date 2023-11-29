5cdc286d39412d53c94765d2afbcde3acd8e187c
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-11-29 17:12:21 +0000

    DCAC-279: Log rerouting of message to invalid topic.

--- src/main/java/uk/gov/companieshouse/ordernotification/consumer/itemgroupprocessedsend/InvalidMessageRouter.java
+++ src/main/java/uk/gov/companieshouse/ordernotification/consumer/itemgroupprocessedsend/InvalidMessageRouter.java
@@ -1,16 +1,22 @@
 package uk.gov.companieshouse.ordernotification.consumer.itemgroupprocessedsend;
 
+import static uk.gov.companieshouse.ordernotification.logging.LoggingUtilsConfiguration.APPLICATION_NAMESPACE;
+
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerInterceptor;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import uk.gov.companieshouse.itemgroupprocessedsend.ItemGroupProcessedSend;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
 
 /**
  * Routes a message to the invalid letter topic if a non-retryable error has been thrown during
  * message processing.
  */
 public class InvalidMessageRouter implements ProducerInterceptor<String, ItemGroupProcessedSend> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(APPLICATION_NAMESPACE);
 
     private MessageFlags messageFlags;
     private String invalidMessageTopic;
@@ -22,6 +28,9 @@ public class InvalidMessageRouter implements ProducerInterceptor<String, ItemGro
             messageFlags.destroy();
             return producerRecord;
         } else {
+            // TODO DCAC-279 Use structured logging
+            LOGGER.info("Producing invalid message " + producerRecord.value() + " to topic "
+                + invalidMessageTopic + ".");
             return new ProducerRecord<>(this.invalidMessageTopic, producerRecord.key(),
                 producerRecord.value());
         }
