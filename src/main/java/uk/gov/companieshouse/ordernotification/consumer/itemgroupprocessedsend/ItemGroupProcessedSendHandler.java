137f8a35f66b75b326c4328a4dddbd80f3ea0320
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-11-30 09:02:07 +0000

    DCAC-279: Remove temporary test code.

--- src/main/java/uk/gov/companieshouse/ordernotification/consumer/itemgroupprocessedsend/ItemGroupProcessedSendHandler.java
+++ src/main/java/uk/gov/companieshouse/ordernotification/consumer/itemgroupprocessedsend/ItemGroupProcessedSendHandler.java
@@ -2,8 +2,6 @@ package uk.gov.companieshouse.ordernotification.consumer.itemgroupprocessedsend;
 
 import static uk.gov.companieshouse.ordernotification.logging.LoggingUtils.getLogMap;
 
-import java.util.Optional;
-import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.itemgroupprocessedsend.ItemGroupProcessedSend;
@@ -22,18 +20,7 @@ public class ItemGroupProcessedSendHandler {
         final ItemGroupProcessedSend payload = message.getPayload();
         logger.info("processing item-group-processed-send message: " + payload, getLogMap(payload));
 
-        errorExceptInDltTopic(message);
-
         // TODO DCAC-295: Send the relevant information onwards via the email-send topic.
     }
 
-    private void errorExceptInDltTopic(final Message<?> incomingMessage) {
-        final String topic = Optional.ofNullable((String) incomingMessage.getHeaders()
-                .get(KafkaHeaders.RECEIVED_TOPIC))
-            .orElse("no topic");
-        if (true) {
-            logger.error("Will throw a NON-retryable exception from topic " + topic);
-            throw new RuntimeException("Thrown from topic " + topic + " to test handling of non-retryable exception", new Exception());
-        }
-    }
 }
