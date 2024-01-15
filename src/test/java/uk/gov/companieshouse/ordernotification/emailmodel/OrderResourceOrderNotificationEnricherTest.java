767330e70b73c42e60bd1afe524acfc72800dbda
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2024-01-15 15:06:31 +0000

    DCAC-295: Test exception handling.

--- src/test/java/uk/gov/companieshouse/ordernotification/emailmodel/OrderResourceOrderNotificationEnricherTest.java
+++ src/test/java/uk/gov/companieshouse/ordernotification/emailmodel/OrderResourceOrderNotificationEnricherTest.java
@@ -7,6 +7,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
+import static uk.gov.companieshouse.ordernotification.fixtures.TestConstants.ITEM_GROUP_PROCESSED_SEND;
 
 import java.util.Collections;
 import org.hamcrest.Matchers;
@@ -62,6 +63,23 @@ class OrderResourceOrderNotificationEnricherTest {
     }
 
     @Test
+    void testThrowExceptionIfOrdersApiErrorsDuringEnrichmentWithItemGroupProcessedSend()
+        throws OrdersResponseException {
+        //given
+        when(orderRetrievable.getOrderData(anyString())).thenThrow(OrdersResponseException.class);
+        when(loggingUtils.logWithOrderUri(any(), any())).thenReturn(Collections.emptyMap());
+
+        //when
+        Executable actual = () -> enricher.enrich(TestConstants.ORDER_NOTIFICATION_REFERENCE,
+            ITEM_GROUP_PROCESSED_SEND);
+
+        //then
+        assertThrows(OrdersResponseException.class, actual);
+        verify(orderRetrievable).getOrderData(TestConstants.ORDER_NOTIFICATION_REFERENCE);
+        verifyNoInteractions(ordersApiMapper);
+    }
+
+    @Test
     void testLogRuntimeExceptionThrownByMapper() {
         //given
         when(orderRetrievable.getOrderData(anyString())).thenReturn(ordersApiWrappable);
@@ -71,6 +89,24 @@ class OrderResourceOrderNotificationEnricherTest {
 
         //when
         Executable actual = () -> enricher.enrich(TestConstants.ORDER_NOTIFICATION_REFERENCE);
+
+        //then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, actual);
+        verify(logger).error("Failed to map order resource", exception, Collections.emptyMap());
+    }
+
+    @Test
+    void testLogRuntimeExceptionThrownDuringEnrichmentWithItemGroupProcessedSend() {
+        //given
+        when(orderRetrievable.getOrderData(anyString())).thenReturn(ordersApiWrappable);
+        when(ordersApiMapper.mapToEmailSend(any(), any())).thenThrow(
+            IllegalArgumentException.class);
+        when(loggingUtils.logWithOrderUri(any(), any())).thenReturn(Collections.emptyMap());
+        when(loggingUtils.getLogger()).thenReturn(logger);
+
+        //when
+        Executable actual = () -> enricher.enrich(TestConstants.ORDER_NOTIFICATION_REFERENCE,
+            ITEM_GROUP_PROCESSED_SEND);
 
         //then
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, actual);
---
+++ src/test/java/uk/gov/companieshouse/ordernotification/itemstatusupdatenotificationsender/ItemGroupProcessedSendSenderServiceTest.java
@@ -0,0 +1,90 @@
+package uk.gov.companieshouse.ordernotification.itemstatusupdatenotificationsender;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+import uk.gov.companieshouse.itemgroupprocessedsend.ItemGroupProcessedSend;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.ordernotification.consumer.orderreceived.RetryableErrorException;
+import uk.gov.companieshouse.ordernotification.emailmodel.OrderResourceOrderNotificationEnricher;
+import uk.gov.companieshouse.ordernotification.emailsender.EmailSend;
+import uk.gov.companieshouse.ordernotification.emailsender.SendEmailEvent;
+import uk.gov.companieshouse.ordernotification.logging.LoggingUtils;
+import uk.gov.companieshouse.ordernotification.messageproducer.MessageProducer;
+
+@ExtendWith(MockitoExtension.class)
+class ItemGroupProcessedSendSenderServiceTest {
+
+    @InjectMocks
+    private ItemGroupProcessedSendSenderService itemGroupProcessedSendSenderService;
+
+    @Mock
+    private MessageProducer producer;
+
+    @Mock
+    private ItemGroupProcessedSend event;
+
+    @Mock
+    private EmailSend emailSendModel;
+
+    @Mock
+    private LoggingUtils loggingUtils;
+
+    @Mock
+    private OrderResourceOrderNotificationEnricher orderEnricher;
+
+    @Mock
+    private Logger logger;
+
+    @Mock
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Test
+    void testHandleEventNoExceptionsThrown() {
+
+        // given
+        when(loggingUtils.getLogger()).thenReturn(logger);
+
+        // when
+        itemGroupProcessedSendSenderService.handleEvent(event);
+
+        // then
+        verify(logger).debug(
+            eq("Successfully enriched item group item status update; notifying email sender"),
+            any());
+
+        verify(applicationEventPublisher).publishEvent(any(SendEmailEvent.class));
+    }
+
+    @Test
+    void testRetryableErrorExceptionIsLoggedAndPropagated() {
+
+        // given
+        when(orderEnricher.enrich(anyString(), eq(event))).thenThrow(
+            new RetryableErrorException("Test exception", new Throwable("Test throwable")));
+        when(loggingUtils.getLogger()).thenReturn(logger);
+
+        // when and then
+        final RetryableErrorException exception = assertThrows(RetryableErrorException.class,
+            () -> itemGroupProcessedSendSenderService.handleEvent(event));
+
+        verify(logger).error(eq("Failed to enrich item group item status update"), eq(exception),
+            anyMap());
+        assertThat(exception.getMessage(), is("Test exception"));
+        assertThat(exception.getCause().getMessage(), is("Test throwable"));
+    }
+
+}
