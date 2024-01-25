f13776d787c95719c69cfb855fc99d0b9366ce19
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2024-01-25 11:37:37 +0000

    DCAC-295: Investigative Kafka configuration changes
    1. Install ErrorHandlingDeserializer for consumer factory to avoid potential poison pill scenarios arising from message deserialization failures.
    2. Install KafkaListenerErrorHandler - experiment not completed
    3. Try to force presence of delivery attempts header
    4. Install @KafkaListeners to spy on the message retry count value tracked by spring-kafka.

--- src/main/java/uk/gov/companieshouse/ordernotification/config/KafkaConfig.java
+++ src/main/java/uk/gov/companieshouse/ordernotification/config/KafkaConfig.java
@@ -3,22 +3,31 @@ package uk.gov.companieshouse.ordernotification.config;
 import static uk.gov.companieshouse.ordernotification.consumer.itemgroupprocessedsend.InvalidMessageRouter.INVALID_MESSAGE_TOPIC;
 import static uk.gov.companieshouse.ordernotification.consumer.itemgroupprocessedsend.InvalidMessageRouter.MESSAGE_FLAGS;
 
+import consumer.deserialization.AvroDeserializer;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.retrytopic.RetryTopicHeaders;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.util.backoff.FixedBackOff;
 import uk.gov.companieshouse.itemgroupprocessedsend.ItemGroupProcessedSend;
 import uk.gov.companieshouse.kafka.exceptions.ProducerConfigException;
@@ -58,11 +67,28 @@ public class KafkaConfig {
             new MessageDeserialiser<>(OrderReceived.class));
     }
 
+//    @Bean
+//    public ConsumerFactory<String, ItemGroupProcessedSend> itemGroupProcessedSendConsumerFactory() {
+//        return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(),
+//            new MessageDeserialiser<>(ItemGroupProcessedSend.class));
+//    }
+
     @Bean
     public ConsumerFactory<String, ItemGroupProcessedSend> itemGroupProcessedSendConsumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(),
-            new MessageDeserialiser<>(ItemGroupProcessedSend.class));
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs2(), new StringDeserializer(),
+            new ErrorHandlingDeserializer<>(new AvroDeserializer<>(ItemGroupProcessedSend.class)));
     }
+//
+//    @Bean
+//    KafkaListenerErrorHandler eh(DeadLetterPublishingRecoverer recoverer) {
+//        return (msg, ex) -> {
+//            if (msg.getHeaders().get(KafkaHeaders.DELIVERY_ATTEMPT, Integer.class) > 9) {
+//                recoverer.accept(msg.getHeaders().get(KafkaHeaders.RAW_DATA, ConsumerRecord.class), ex);
+//                return "FAILED";
+//            }
+//            throw ex;
+//        };
+//    }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, OrderReceived> kafkaOrderReceivedListenerContainerFactory() {
@@ -77,6 +103,7 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, ItemGroupProcessedSend> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(itemGroupProcessedSendConsumerFactory());
         factory.setCommonErrorHandler(new DefaultErrorHandler(new FixedBackOff(0, 0)));
+        // factory.getContainerProperties().setDeliveryAttemptHeader(true);
         return factory;
     }
 
@@ -85,6 +112,21 @@ public class KafkaConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerAddresses);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, MessageDeserialiser.class);
+        return props;
+    }
+
+    private Map<String, Object> consumerConfigs2() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerAddresses);
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, MessageDeserialiser.class);
+
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, AvroDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
         return props;
     }
 
@@ -168,5 +210,25 @@ public class KafkaConfig {
     public KafkaTemplate<String, ItemGroupProcessedSend> kafkaTemplate(
         ProducerFactory<String, ItemGroupProcessedSend> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
+    }
+
+    @KafkaListener(id = "item-group-processed-send", topics = "item-group-processed-send")
+    void listen(String in, @Header(name = RetryTopicHeaders.DEFAULT_HEADER_ATTEMPTS, required = false) Integer attempts) {
+        logger.info("listen(" + in + ", " + attempts + ")");
+    }
+
+    @KafkaListener(id = "item-group-processed-send-retry", topics = "item-group-processed-send-retry")
+    void listenRetry(String in, @Header(name = RetryTopicHeaders.DEFAULT_HEADER_ATTEMPTS, required = false) Integer attempts) {
+        logger.info("listenRetry(" + in + ", " + attempts + ")");
+    }
+
+    @KafkaListener(id = "item-group-processed-send-error", topics = "item-group-processed-send-error")
+    void listenError(String in, @Header(name = RetryTopicHeaders.DEFAULT_HEADER_ATTEMPTS, required = false) Integer attempts) {
+        logger.info("listenError(" + in + ", " + attempts + ")");
+    }
+
+    @KafkaListener(id = "item-group-processed-send-invalid", topics = "item-group-processed-send-invalid")
+    void listenInvalid(String in, @Header(name = RetryTopicHeaders.DEFAULT_HEADER_ATTEMPTS, required = false) Integer attempts) {
+        logger.info("listenInvalid(" + in + ", " + attempts + ")");
     }
 }
