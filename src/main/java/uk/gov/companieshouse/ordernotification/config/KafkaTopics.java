276e3d0852b8af511ffa4322a8d14c22f138d2c6
jbishop2-ch <jbishop2@companieshouse.gov.uk>
2025-05-29 08:35:14 +0100

    Bumped versions of Private SDK for sending emails

--- src/main/java/uk/gov/companieshouse/ordernotification/config/KafkaTopics.java
+++ src/main/java/uk/gov/companieshouse/ordernotification/config/KafkaTopics.java
@@ -1,15 +1,11 @@
 package uk.gov.companieshouse.ordernotification.config;
 
 public class KafkaTopics {
-    private String emailSend;
+
     private String orderReceived;
     private String orderReceivedRetry;
     private String orderReceivedError;
     private String itemGroupProcessedSend;
-
-    public void setEmailSend(String emailSend) {
-        this.emailSend = emailSend;
-    }
 
     public void setOrderReceived(String orderReceived) {
         this.orderReceived = orderReceived;
@@ -25,10 +21,6 @@ public class KafkaTopics {
 
     public void setItemGroupProcessedSend(String itemGroupProcessedSend) {
         this.itemGroupProcessedSend = itemGroupProcessedSend;
-    }
-
-    public String getEmailSend() {
-        return emailSend;
     }
 
     public String getOrderReceived() {
